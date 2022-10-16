548e4296eb6c4c4916dee36332ecc512d4f98c53
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2022-10-16 11:08:05 +0530

    carousal responsive

--- JS/carousel.js
+++ JS/carousel.js
@@ -5,13 +5,15 @@ $(document).ready(function () {
     let next = cur.next();
     let nextBorder = curBorder.next();
 
-    console.log(cur, next);
-
     if (next.length) {
       cur.removeClass("active carousal-border").css("z-index", -10);
       next.addClass("active carousal-border").css("z-index", +10);
-      curBorder.removeClass(" carousal-border");
-      nextBorder.addClass("carousal-border");
+      curBorder
+        .removeClass("carousal-border")
+        .css("border-bottom", "2px solid white");
+      nextBorder
+        .addClass("carousal-border")
+        .css("border-bottom", "2px solid black");
     }
   });
   $(".prev").click(() => {
@@ -24,8 +26,40 @@ $(document).ready(function () {
     if (prev.length) {
       cur.removeClass("active").css("z-index", -10);
       prev.addClass("active").css("z-index", +10);
-      curBorder.removeClass(" carousal-border");
-      prevBorder.addClass("carousal-border");
+      curBorder
+        .removeClass(" carousal-border")
+        .css("border-bottom", "2px solid white");
+      prevBorder
+        .addClass("carousal-border")
+        .css("border-bottom", "2px solid black");
     }
   });
+  $(".prev").hover(
+    () => {
+      $(".prev").css({
+        "background-color": "white",
+        color: "black",
+        border: "2px dotted black",
+        position: "relative",
+      });
+    },
+    () => {
+      $(".prev").css({
+        "background-color": "black",
+        color: "white",
+      });
+    }
+  );
+  $(".next").hover(
+    () => {
+      $(".next").css({
+        "background-color": "white",
+        color: "black",
+        border: "2px dotted black",
+      });
+    },
+    () => {
+      $(".next").css({ "background-color": "black", color: "white" });
+    }
+  );
 });
